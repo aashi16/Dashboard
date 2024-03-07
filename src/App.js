26c6302bb10e2d44548d527aae5e7b1b0687c579
Aashi singh <asheesingh16@gmail.com>
2024-03-07 00:57:22 -0500

    Fix For mobile UI

--- src/App.js
+++ src/App.js
@@ -1,34 +1,26 @@
 import './App.css';
-import {  useState, useEffect } from 'react';
+import {  useState } from 'react';
+
 // import axios from 'axios'
 import Menubar from './Components/Menubar.js'
 import Data from './Data/SampleData.js'
 import Layout from './Components/Layout.js';
 import Navbar from './Components/Navbar.js';
 import DashboardContext from './Helpers/Dashboardcontext.js';
-import useScreenSize from './Components/useScreenSize.js';
+
 function App() {
-  const screenSize = useScreenSize();
-  const [isMobile, setIsMobile] = useState();
   const [itemSelected,setItemSelected] = useState('');
   const [rescale,setRescale] = useState(false);
   const [darkTheme, setDarkTheme] = useState(false);
-  const [mobileNav, setMobileNav] = useState(false);
-
-  useEffect(()=>{
-     setIsMobile(screenSize.width < 1030)
-  })
   const clickedItem = (item) => {
     setItemSelected(item);
   }
-  const showNavbar = (val) => {
-    setMobileNav(val);
-  }
+
   return (
-<div className={`App ${rescale ? 'expand' : 'collapse'} ${isMobile ? 'mobileView' : ''} ${!mobileNav && isMobile ? 'hideNavbar' : 'showNavbar'} ${darkTheme ? 'darkTheme': ''}`}>
-      <DashboardContext.Provider value={{rescale,setRescale,darkTheme,setDarkTheme,isMobile,setIsMobile}}>
+<div className={`App ${rescale ? 'fullWidth' : ''} ${darkTheme ? 'darkTheme': ''}`}>
+      <DashboardContext.Provider value={{rescale,setRescale,darkTheme,setDarkTheme}}>
       <Menubar menulist={Data.menuList} selectedItem={clickedItem}/>
-      <Navbar showNavbar={showNavbar}/>
+      <Navbar />
       <Layout itemSelected={itemSelected} />
       </DashboardContext.Provider>
       
